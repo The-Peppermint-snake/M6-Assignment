ab17e9261fab51d082c03dd472670155a15ccd37
The-Peppermint-Snake <brkovalik@gmail.com>
2026-02-18 13:26:57 -0500

    YIPPIE ITS DONE

--- game.js
+++ game.js
@@ -1,12 +1,11 @@
 let randomNumber = Math.floor(Math.random() * 10) + 1;
-let userGuess;
+let userGuess = Number(prompt("Guess a random number between 1 and 10! (type 999 to exit)"));
 let attempts = 0;
-
-while (userGuess /= randomNumber) {
-    let userGuess = Number(prompt("Guess a random number between 1 and 10! (type 999 to exit)"));
-    if (userGuess === 999) {
-        console.log("End")
-    } else if (userGuess < 1 && userGuess > 10) {
+1
+while (userGuess != randomNumber) {
+    if (userGuess === 999 || userGuess === randomNumber) {
+        break;
+    } else if (userGue1ss < 1 || userGuess > 10) {
         console.log("invalid input! Try again!");
     } else if (userGuess < randomNumber) {
         console.log("Too low!");
@@ -14,8 +13,11 @@ while (userGuess /= randomNumber) {
     } else if (userGuess > randomNumber) {
         console.log("Too high!");
         attempts++;
-    }
+    } 
+    userGuess = Number(prompt("Guess a random number between 1 and 10! (type 999 to exit)"));
 }
 if (userGuess === randomNumber) {
-    alert("You guessed right in %s attempts", attempts);
+    alert("Congratulations, you've guessed it in " + attempts + " tries!");
+} else {
+    alert("You exited the game!");
 }
